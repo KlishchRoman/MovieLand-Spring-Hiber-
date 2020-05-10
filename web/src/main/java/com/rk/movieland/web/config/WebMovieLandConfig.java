5d74e134c8f4f9eb8257330e28600b20e7f707fd
KlishchRoman <klisch.roma@gmail.com>
2020-05-10 15:33:11 +0300

    add file appender

--- web/src/main/java/com/rk/movieland/web/config/WebMovieLandConfig.java
+++ web/src/main/java/com/rk/movieland/web/config/WebMovieLandConfig.java
@@ -1,5 +1,7 @@
 package com.rk.movieland.web.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -11,21 +13,24 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 @ComponentScan(basePackages = "com.rk.movieland.web")
 public class WebMovieLandConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LogManager.getRootLogger();
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        logger.debug("Test WebMovieLandConfig");
         configurer.mediaType("json", MediaType.APPLICATION_JSON);
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
-
     }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+        logger.debug("Test WebMovieLandConfig");
         registry.enableContentNegotiation(new MappingJackson2JsonView());
 
     }
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        logger.debug("Test WebMovieLandConfig");
         configurer.enable();
     }
 }
--- web/src/main/java/com/rk/movieland/web/controller/MovieController.java
+++ web/src/main/java/com/rk/movieland/web/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.rk.movieland.web.controller;
 
 import com.rk.movieland.domain.Movie;
 import com.rk.movieland.service.MovieService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +16,20 @@ import java.util.List;
 @RequestMapping(value = "/v1/movie", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MovieController {
 
+    private static final Logger logger = LogManager.getRootLogger();
+
     @Autowired
     MovieService movieService;
 
     @GetMapping
     public List<Movie> getAllMovie() {
+        logger.debug("Test MovieController");
         return movieService.getAllMovies();
     }
 
     @GetMapping(value = "/random")
     public List<Movie> get3RandomMovie() {
+        logger.debug("Test MovieController");
         return movieService.get3RanndomMovies();
     }
 }
